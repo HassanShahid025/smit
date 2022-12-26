1233ac52636fa032ace730790517b07aab2bd9d5
HassanShahid025 <hassanshahid234@gmail.com>
2022-12-26 22:53:06 +0500

    cards fixed

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, CardMedia, Container, Grid, Typography } from '@mui/material';
+import AboutCards from './Card';
 import * as React from 'react';
 import './About.css'
 import aboutImg from '../images/about3.jpg'
@@ -13,52 +14,11 @@ const About = () => {
                     <img src={aboutImg} alt="image" className='about-img'/>
                 </div >
                 <div className='about-text'>
-                    <h1>PROVIDING QUALITY EDUCATION</h1>
+                    <h1>PROVIDING QUALITY  FREE EDUCATION TO EVERYONE </h1>
                     <Typography variant="body1">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     </Typography>
-                    <div className='card-container'>
-                        <Card sx={{ maxWidth: 250,backgroundColor:'red' }}>
-                            <CardContent>
-                                <Typography variant="h4" color="white" textAlign='center'>
-                                10+
-                                </Typography>
-                                <Typography variant="h4" color="white">
-                                Courses
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                        <Card sx={{ maxWidth: 150,backgroundColor:'green' }}>
-                            <CardContent>
-                                <Typography variant="h4" color="white" textAlign='center'>
-                                10+
-                                </Typography>
-                                <Typography variant="h4" color="white">
-                                Courses
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                        <Card sx={{ maxWidth: 150,backgroundColor:'green' }}>
-                            <CardContent>
-                                <Typography variant="h4" color="white" textAlign='center'>
-                                10+
-                                </Typography>
-                                <Typography variant="h4" color="white">
-                                Courses
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                        <Card sx={{ maxWidth: 250,backgroundColor:'green' }}>
-                            <CardContent>
-                                <Typography variant="h4" color="white" textAlign='center'>
-                                10+
-                                </Typography>
-                                <Typography variant="h4" color="white">
-                                Courses
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                   </div>
+                    <AboutCards/>
                 </div>
             </div>
         </Container>
---
+++ src/components/about/Card.tsx
@@ -0,0 +1,50 @@
+import { Card, CardContent, Typography } from "@mui/material"
+
+const AboutCards = () =>{
+    return(
+        <div className='card-container'>
+            <Card sx={{ maxWidth: 250,backgroundColor:'red' }}>
+                <CardContent>
+                    <Typography variant="h4" color="white" textAlign='center'>
+                    10+
+                    </Typography>
+                    <Typography variant="h4" color="white">
+                    Courses
+                    </Typography>
+                </CardContent>
+            </Card>
+            <Card sx={{ maxWidth: 150,backgroundColor:'green' }}>
+                <CardContent>
+                    <Typography variant="h4" color="white" textAlign='center'>
+                    10+
+                    </Typography>
+                    <Typography variant="h4" color="white">
+                    Courses
+                    </Typography>
+                </CardContent>
+            </Card>
+            <Card sx={{ maxWidth: 150,backgroundColor:'blue' }}>
+                <CardContent>
+                    <Typography variant="h4" color="white" textAlign='center'>
+                    10+
+                    </Typography>
+                    <Typography variant="h4" color="white">
+                    Courses
+                    </Typography>
+                </CardContent>
+            </Card>
+            <Card sx={{ maxWidth: 250,backgroundColor:'yellow' }}>
+                <CardContent>
+                    <Typography variant="h4" color="white" textAlign='center'>
+                    10+
+                    </Typography>
+                    <Typography variant="h4" color="white">
+                    Courses
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
+
+export default AboutCards;
