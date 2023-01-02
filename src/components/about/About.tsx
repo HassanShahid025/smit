5623468678af665ab3717dcd1b15cf82b960c16c
HassanShahid025 <hassanshahid234@gmail.com>
2023-01-02 18:01:54 +0500

    about2 added

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -3,24 +3,28 @@ import AboutCards from './Card';
 import * as React from 'react';
 import './About.css'
 import aboutImg from '../images/about3.jpg'
+import About2 from './About2';
 
 
 const About = () => {
     return(
         <Container sx={{marginY:"2rem"}}>
-            <Typography variant='h3' component='h1' textAlign='center' sx={{marginBottom:'1rem'}}>About</Typography>
-            <div className='about-conatiner'>
-                <div className='about-img-container'>
-                    <img src={aboutImg} alt="image" className='about-img'/>
-                </div >
-                <div className='about-text'>
-                    <h1>PROVIDING QUALITY  FREE EDUCATION TO EVERYONE </h1>
-                    <Typography variant="body1">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </Typography>
-                    <AboutCards/>
+            <Container sx={{marginY:"2rem"}}>
+                <Typography variant='h3' component='h1' textAlign='center' sx={{marginBottom:'1rem'}}>About</Typography>
+                <div className='about-conatiner'>
+                    <div className='about-img-container'>
+                        <img src={aboutImg} alt="image" className='about-img'/>
+                    </div >
+                    <div className='about-text'>
+                        <h1>PROVIDING QUALITY  FREE EDUCATION TO EVERYONE </h1>
+                        <Typography variant="body1">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </Typography>
+                        <AboutCards/>
+                    </div>
                 </div>
-            </div>
+            </Container>
+            <About2/>
         </Container>
     )}
 
