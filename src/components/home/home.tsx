d9fc87d7f38449c6ba8d23fcfd05e148d2c52cab
HassanShahid025 <hassanshahid234@gmail.com>
2023-01-15 07:28:25 +0500

    news section started

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -3,6 +3,7 @@ import Carousel from '../carousel/ImageSlider'
 import Contact from '../contact/Contact';
 import { FbCourses } from '../courses/FirebaseCourses';
 import ResponsiveAppBar from '../navBar/Navbar';
+import News from '../news/News';
 import RegistrationForm from '../RegistrationForm/RegistrationForm';
 
 
@@ -11,6 +12,7 @@ function Home() {
   return (
     <div>
       <Carousel/>
+      <News/>
       <FbCourses/>
       <About/> 
       <Contact/>   
